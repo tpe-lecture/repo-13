e3bc09e18bd0c7a8e2d407b1608862f55ffc8612
kurnizki77 <kurnizkij@gmail.com>
2016-12-05 11:46:32 +0100

    Lambda

--- 09_geschachtelte_klassen/06_lambdas/src/tpe/nested/lambda/Main.java
+++ 09_geschachtelte_klassen/06_lambdas/src/tpe/nested/lambda/Main.java
@@ -22,11 +22,14 @@ public class Main {
 
         // TODO: Gerade Zaheln filtern
         int[] gerade = null;
-
+        Predicate<Integer> x = (a) -> a%2==0;
+        gerade =s.filter(x, ZAHLEN);
         System.out.println(Arrays.toString(gerade));
 
         // TODO: Ungerade Zaheln filtern
         int[] ungerade = null;
+        Predicate<Integer> y = (a) -> a%2==1;
+        ungerade =s.filter(y, ZAHLEN);
 
         System.out.println(Arrays.toString(ungerade));
 
